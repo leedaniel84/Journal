da59e8065b2cc90f825022b35ee08e60ad865db5
Daniel Lee <lee.daniel84@gmail.com>
2015-10-15 21:04:10 -0600

    Part 1 and Part2
    Part 2 partially done.

---
+++ Journal/EntryDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Daniel Lee on 10/15/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    var entry: Entry?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+    @IBAction func clearButtonPressed(sender: AnyObject) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
